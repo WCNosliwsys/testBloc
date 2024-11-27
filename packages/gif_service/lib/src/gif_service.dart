f85eb47d3930e7e9d683cb4ff206c5d12476b995
WIL <nosliwys@gmail.com>
2024-11-27 16:41:07 -0500

    implementando gif service recupera links http

--- packages/gif_service/lib/src/gif_service.dart
+++ packages/gif_service/lib/src/gif_service.dart
@@ -1,7 +1,40 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 /// {@template gif_service}
 /// My Gif Service
 /// {@endtemplate}
 class GifService {
   /// {@macro gif_service}
-  const GifService();
+
+  GifService();
+  final http.Client _httpClient = http.Client();
+  final String baseUrl = "api.giphy.com";
+  final String endPoint = "/v1/gifs/trending";
+  final String apiKey = '16JI47HdOqG8RQ3C6o9FyosbEhEkCH7h';
+
+  Future<List<String>> fetchGifs() async {
+    final uri = Uri.http(baseUrl, endPoint, {'api_key': apiKey});
+    http.Response response;
+    List body;
+    try {
+      response = await _httpClient.get(uri);
+    } on Exception {
+      throw Exception();
+    }
+    if (response.statusCode != 200) {
+      throw HttpRequestException();
+    }
+    try {
+      body = jsonDecode(response.body)['data'] as List;
+    } on Exception {
+      throw JsonDecodeException();
+    }
+   return  body.map((url) => url['images']['original']['url'].toString()).toList();
+  }
 }
+
+class HttpRequestException implements Exception {}
+
+class JsonDecodeException implements Exception {}
